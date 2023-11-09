a901bde2b61bdaa92871a2223829809d0865daf6
unknown <emonhossen1000@gmail.com>
2023-11-09 11:23:46 +0600

    connected to the mongo db data bas

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const express = require("express");
-const app = express();
 let cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
+// cors
 app.use(cors());
 app.use(express.json());
+
+//8KfKU1cEyCXpXsAx
+//serviceMaster
+
+//******************************************************* */
+
+const uri =
+  "mongodb+srv://serviceMaster:8KfKU1cEyCXpXsAx@cluster0.r0kjzd3.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//******************************************************* */
 
 app.get("/", (req, res) => {
   res.send("This is service sharing server site");
