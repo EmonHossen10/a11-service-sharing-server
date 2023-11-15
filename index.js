39f8bc0dda554a1ed55afe0e5de56f5e27c78d8f
unknown <emonhossen1000@gmail.com>
2023-11-16 00:43:26 +0600

    update route added successfully

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // cors
+app.use(
+  cors({
+    otigin: [
+      // "http://localhost:5173/"
+      "https://service-master-9864e.web.app/",
+      "https://service-master-9864e.firebaseapp.com/",
+    ],
+    credentials: true,
+  })
+);
 app.use(cors());
 app.use(express.json());
 
@@ -38,11 +48,14 @@ async function run() {
       .collection("bookings");
     const addService = client.db("serviceMasterDB").collection("addService");
 
+    // -- 01
+
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    // -- 02
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
@@ -52,27 +65,68 @@ async function run() {
       res.send(result);
     });
 
-    // --04 add services
+    // --03 add services
     app.post("/addservices", async (req, res) => {
       const newService = req.body;
       const result = await addService.insertOne(newService);
       res.send(result);
     });
+    // -- 04 show service with query
 
     app.get("/showAddService", async (req, res) => {
-      const cursor = addService.find();
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = addService.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // -- 05
+
+    app.delete("/showAddService/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(id);
+      const result = await addService.deleteOne(query);
       res.send(result);
     });
 
     app.get("/showAddService/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await addService.deleteOne(query);
+      console.log("id from ", id);
+      const result = await addService.findOne(query);
       res.send(result);
     });
 
-    // some bookings
+    // update add service
+    app.put("/showAddService/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedService = req.body;
+        console.log("updated", updatedService);
+        const update = {
+          $set: {
+            Image: updatedService.pic,
+            Name: updatedService.name,
+            description: updatedService.description,
+            serviceArea: updatedService.area,
+            Price: updatedService.price,
+          },
+        };
+        const result = await addService.updateOne(filter, update);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // -- 07 some bookings
     app.get("/bookings", async (req, res) => {
       console.log(req.query);
       let query = {};
@@ -82,15 +136,8 @@ async function run() {
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
     });
-    app.get("/showAddService", async (req, res) => {
-      console.log(req.query);
-      let query = {};
-      if (req.query?.email) {
-        query = { email: req.query.email };
-      }
-      const result = await addService.find(query).toArray();
-      res.send(result);
-    });
+
+    // -- 08
 
     app.post("/addbookings", async (req, res) => {
       const newBooking = req.body;
